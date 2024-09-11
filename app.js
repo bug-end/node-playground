689afc15ccd24caa22e8e8ee373e2c0e38286e94
lukasz <l.bielecki13@gmail.com>
2024-09-11 21:35:47 +0200

    adding express router

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-  console.log('In the middleware!', req.url);
-});
+app.use(express.json());
+
+// Built-in middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
