b6c4c05da1a8fe38dc70f5fa51cbee0e0edacb0d
lukasz <l.bielecki13@gmail.com>
2024-09-16 21:37:28 +0200

    adding 404 page

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
