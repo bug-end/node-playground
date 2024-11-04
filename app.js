f437a8ef709558eb500e37ae5e8744b9d2a17a22
lukasz <l.bielecki13@gmail.com>
2024-11-04 20:49:10 +0100

    installing and implementing pug

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Set the view engine to pug
+app.set('view engine', 'pug');
+
 // Import route handlers
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // Enable parsing of JSON data in request bodies
@@ -17,11 +20,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(port, () => {
