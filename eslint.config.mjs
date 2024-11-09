1b425bd9277f681ada10c010b224dab6f9488737
lukasz <l.bielecki13@gmail.com>
2024-11-09 19:02:14 +0100

    reinstalling eslint and removing html files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,9 @@
 import globals from 'globals';
-import eslintJs from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
+import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      sourceType: 'commonjs',
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    },
-    plugins: {
-      prettier: eslintPluginPrettier
-    },
-    rules: {
-      'prettier/prettier': 'error'
-    },
-    ignores: ['node_modules', 'dist']
-  },
-  eslintJs.configs.recommended,
-  eslintConfigPrettier
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended
 ];
