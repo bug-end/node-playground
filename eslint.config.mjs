7df7d8586769a117c8b36ec387b3cbc97a1e21f0
lukasz <l.bielecki13@gmail.com>
2024-11-09 20:27:35 +0100

    changing prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,5 @@ import pluginJs from '@eslint/js';
 export default [
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended
+  pluginJs.configs.recommended,
 ];
