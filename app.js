29b982dbb2ef96ee6671f1dc16fd9c9c632f6987
lukasz <l.bielecki13@gmail.com>
2024-11-11 20:35:30 +0100

    updating ejs files to use includes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: '404 Page Not Found' });
+  res.status(404).render('404', { pageTitle: '404 Page Not Found', path: req.url });
 });
 
 app.listen(port, () => {
