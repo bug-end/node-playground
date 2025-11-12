54b88f796fe15530aab751907483d0453362bd0f
lukasz <l.bielecki13@gmail.com>
2025-11-12 22:47:09 +0100

    feat(product): update product fetching logic

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,9 +6,16 @@ exports.getIndex = (req, res, next) => {
   });
 };
 
-exports.getProducts = (req, res) => {
+exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('shop/product-list', { prods: products, pageTitle: 'All Products', path: '/products' });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    console.log(product);
   });
 };
 
